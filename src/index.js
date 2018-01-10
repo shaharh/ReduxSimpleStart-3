4e6caff1e4c4ff9e200e6664a142576196e6ed32
Shahar <shaharblues@yahoo.com>
2018-01-10 15:11:32 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+class Hello extends React.Component {
+  render(){return <div>Hello</div>}
+}
+
+class World extends React.Component {
+  render(){return <div>World</div>}
+}
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Route path="/hello" component={Hello} />
+        <Route path="/world" component={World} />
+      </div>
+    </BrowserRouter>
+  </Provider>
+  , document.querySelector('.container'));
