eb8ae2fe094abe8bc6097c1a5cc312ef29862cae
shaharh <shaharblues@yahoo.com>
2018-01-12 09:34:35 +0200

    finaly finished yay

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,4 +1,4 @@
-import { FETCH_POST, FETCH_POSTS} from '../actions';
+import { FETCH_POST, FETCH_POSTS, DELETE_POST} from '../actions';
 import _ from 'lodash';
 
 export default function(state = {}, action) {
@@ -7,10 +7,9 @@ export default function(state = {}, action) {
       return _.mapKeys(action.payload.data, 'id');
     case FETCH_POST:
       const post = action.payload.data;
-      // const newState = {...state, };
-      // newState[post.id] = post;
-      // return newState;
       return {...state, [action.payload.data.id]:action.payload.data};
+    case DELETE_POST:
+      return _.omit(state, action.payload);
     default:
       return state;
   }
